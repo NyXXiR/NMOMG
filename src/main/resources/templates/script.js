30c2c1a923ef7d062c31495867e7300553ba44f6
ruukr41 <fourfirst1@gmail.com>
2023-02-23 14:41:53 +0900

    -by han

--- src/main/resources/templates/script.js
+++ src/main/resources/templates/script.js
@@ -1,5 +1,4 @@
 const getButton = document.getElementById('get');
-<<<<<<< HEAD
 const multiInput = document.querySelector('multi-input');
 const values = document.querySelector('#values');
 
@@ -8,36 +7,19 @@ getButton.onclick = () => {
 
   //  values.textContent = `Got ${multiInput.getValues().join(' and ')}!`;
 
-=======
+
 const multiInput = document.querySelector('multi-input'); 
 const values = document.querySelector('#values'); 
 
 getButton.onclick = () => {
   if (multiInput.getValues().length > 0) {
-  
-  //  values.textContent = `Got ${multiInput.getValues().join(' and ')}!`;
-  
->>>>>>> origin/main
-  var stack= document.getElementById('stack');
-  stack.value= multiInput.getValues();
+    var stack = document.getElementById('stack');
+    stack.value = multiInput.getValues();
 
-  alert("사용 스택이 등록되었습니다.");
-  //셀렉트 해주는 친구
-<<<<<<< HEAD
+    alert("사용 스택이 등록되었습니다.");
 
-
- //  multiInput.getValues();
   } else {
-   // values.textContent = '아무것도 선택하지 않음';
-=======
-  
-  
- //  multiInput.getValues();
-  } else {
-   // values.textContent = '아무것도 선택하지 않음'; 
->>>>>>> origin/main
-   alert("아무것도 선택하지 않음");
+    alert("아무것도 선택하지 않음");
   }
 }
-
-//document.querySelector('input').focus();
+}
