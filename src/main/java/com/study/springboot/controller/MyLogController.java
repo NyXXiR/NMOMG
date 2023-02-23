2595b2b0412947e2eb158f7b6537779f2de15b8f
ruukr41 <fourfirst1@gmail.com>
2023-02-23 14:31:12 +0900

    -by han

--- src/main/java/com/study/springboot/controller/MyLogController.java
+++ src/main/java/com/study/springboot/controller/MyLogController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.study.springboot.dao.MyLogDao;
@@ -102,7 +101,7 @@ public class MyLogController {
 		
 		@GetMapping("/index")
 		public String index() {
-			return "index";
+			return "home";
 		}
 		
 		
